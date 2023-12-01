1f44bf0bb96a57adbb785dfb83e0aeed69feaa11
Sean Au Jong <seana@ziprecruiter.com>
2023-12-01 11:39:11 -0800

    feat: day01 part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -9,13 +9,50 @@ fun main() {
         return input.sumOf { getCalibrationValue(it) }
     }
 
+    val wordsDigits = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
+
+    fun getRealCalibrationValue(s: String): Int {
+        val initialFirst = s.find { it.isDigit() }
+        val initialLast = s.findLast { it.isDigit() }
+
+        var first = initialFirst?.digitToInt() ?: 0
+        var firstIndex = s.indexOfFirst { it == initialFirst }
+        var last = initialLast?.digitToInt() ?: 0
+        var lastIndex = s.indexOfLast { it == initialLast }
+
+        for ((word, digit) in wordsDigits) {
+            val i = s.indexOf(word)
+            val j = s.lastIndexOf(word)
+            if (i != -1 && (i < firstIndex || firstIndex == -1)) {
+                first = digit
+                firstIndex = i
+            }
+            if (j != -1 && (j > lastIndex || lastIndex == -1)) {
+                last = digit
+                lastIndex = j
+            }
+        }
+
+        return first.times(10).plus(last)
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { getRealCalibrationValue(it) }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
