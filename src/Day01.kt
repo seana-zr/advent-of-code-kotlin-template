933fb06ff0ff323cdafb309d9f6c90223cdfccc2
Sean Au Jong <seana@ziprecruiter.com>
2023-12-01 11:10:48 -0800

    feat: day01 part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,12 @@
 fun main() {
+    fun getCalibrationValue(s: String): Int {
+        val first = s.find { it.isDigit() }!!.digitToInt()
+        val last = s.findLast { it.isDigit() }!!.digitToInt()
+        return first.times(10).plus(last)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { getCalibrationValue(it) }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +15,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
